ad04f4e71bc4373b11ac4e1034e1bdf8bccd45d9
Alan <alanmorua8@gmail.com>
2022-12-23 11:26:44 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,67 @@
+const { createApp } = Vue
+createApp({
+    data() {
+        return {
+        }
+    },
+    created() {
+
+    },
+    methods: {
+        lightbank() {
+            Swal.fire({
+                width: 600,
+                padding: '1em',
+                background: '#252525',
+                html: '<h2 >LightBank</h2>' +
+                    '<h5 style="text-align: start;" class=" text-white">Category: Homebanking</h5>' +
+                    '<h5 style="text-align: start;" class=" text-white">Start Date: 21/09/2022</h5>' +
+                    '<h5 style="text-align: start;" class=" text-white">End Date: 22/11/2022</h5>' +
+                    '<h5 style="text-align: start;" class=" text-white">Repository: <a target="_blank" href="https://github.com/AlanM-ARG/LightBank">GitHub</a></h5>' +
+                    '<h5 style="text-align: start;" class=" text-white">Teamwork: No</h5>' +
+                    '<h5 style="text-align: start;" class=" text-white">Video URL: </h5>' +
+                    '<h5 style="text-align: start;" class="d-flex justify-content-start align-items-center gap-2 flex-wrap text-white">Technologies: <img ' +
+                    'src="./src/Captura de pantalla (190).png" style="width:100%;"  alt="Technologies">'
+            })
+        },
+        indiaPetShop() {
+            Swal.fire({
+                width: 600,
+                padding: '1em',
+                background: '#252525',
+                html: '<h2 >India Petshop</h2>' +
+                    '<h5 style="text-align: start;" class=" text-white">Category: e-Commerce</h5>' +
+                    '<h5 style="text-align: start;" class=" text-white">Start Date: 16/09/2022</h5>' +
+                    '<h5 style="text-align: start;" class=" text-white">End Date: 20/09/2022</h5>' +
+                    '<h5 style="text-align: start;" class=" text-white">Repository: <a target="_blank" href="https://github.com/AlanM-ARG/indiapetshop">GitHub</a></h5>' +
+                    '<h5 style="text-align: start;" class=" text-white">Teamwork: Yes</h5>' +
+                    '<h5 style="text-align: start;" class=" text-white">Web URL: <a href="https://indiapetshop.netlify.app/index.html" target="_blank">India PetShop</a></h5>' +
+                    '<h5 style="text-align: start;" class="d-flex justify-content-start align-items-center gap-2 flex-wrap text-white">Technologies: <img ' +
+                    'src="./src/Captura de pantalla (191).png" style="width:100%;"  alt="Technologies">'
+            })
+        },
+        theWinest() {
+            Swal.fire({
+                width: 600,
+                padding: '1em',
+                background: '#252525',
+                html: '<h2 >The Winest</h2>' +
+                    '<h5 style="text-align: start;" class=" text-white">Category: e-Commerce</h5>' +
+                    '<h5 style="text-align: start;" class=" text-white">Start Date: 24/11/2022</h5>' +
+                    '<h5 style="text-align: start;" class=" text-white">End Date: 02/12/2022</h5>' +
+                    '<h5 style="text-align: start;" class=" text-white">Repository: <a target="_blank" href="https://github.com/AlanM-ARG/wines">GitHub</a></h5>' +
+                    '<h5 style="text-align: start;" class=" text-white">Teamwork: Yes</h5>' +
+                    '<h5 style="text-align: start;" class=" text-white">Video URL: <a href="https://www.youtube.com/watch?v=VcxdUpXM8PM" target="_blank">YouTube</a></h5>' +
+                    '<h5 style="text-align: start;" class="d-flex justify-content-start align-items-center gap-2 flex-wrap text-white">Technologies: <img ' +
+                    'src="./src/Captura de pantalla (192).png" style="width:100%;" alt="Technologies">'
+            })
+        },
+    },
+
+    computed: {
+
+    },
+    mounted() {
+
+    },
+}).mount('#app')
